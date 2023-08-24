77ce3c7652762efe75f3b2485e1467f483dcc15b
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-08-24 11:13:33 +0300

    Done

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name="Guido"):
+        self.name = name
+
+# Create an instance of the Person class with the default name "Guido"
+default_person = Person()
+print(f"Default person's name is {default_person.name}")
+
+# Create an instance of the Person class with a specific name
+custom_person = Person("Alice")
+print(f"Custom person's name is {custom_person.name}")
